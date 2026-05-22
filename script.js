44d16c3e0c878590882e8e603bbdd012355c46dc
MikhailP-C <prokoshinmisha@gmail.com>
2026-05-22 19:32:29 +0300

    more films

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const swiper = new Swiper('.swiper', {
-    // Направление прокрутки: 'horizontal' или 'vertical'
     direction: 'horizontal',
-    loop: false, // Бесконечная прокрутка
-  
-    // Пагинация (точки)
+    loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-  
-    // Кнопки навигации
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // Скроллбар
+
     scrollbar: {
       el: '.swiper-scrollbar',
     },
